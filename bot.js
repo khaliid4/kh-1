8bf0b8d4d86ef7cecd3bfe60e28ed809bb59b4f7
خالد الرشيدي <42046412+khaliid4@users.noreply.github.com>
2019-01-09 14:50:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1233,7 +1233,7 @@ client.on('message', message => {
 
 ////////////////////////////////////////كود البنق مطور/////////////////////////////
 
- client.on('message', message => { //ping
+ client.on('message', message => { 
                                 if(!message.channel.guild) return;
                         if (message.content.startsWith(prefix + 'ping')) {
                             if(!message.channel.guild) return;
@@ -1294,7 +1294,7 @@ client.on('message', message => {
 
           client.on('message', message => {
             let args = message.content.split(' ').slice(1);
-            if(message.content.split(' ')[0] == `{prefix}لون`){
+            if(message.content.split(' ')[0] == `${prefix}لون`){
             const embedd = new Discord.RichEmbed()
             .setFooter('Requested by '+message.author.username, message.author.avatarURL)
             .setDescription(`**لا يوجد لون بهذا الأسم ** ❌ `)
